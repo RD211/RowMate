759fd6165b839ee2e8a96492caf0b98bb6e295ba
Stiliyan Nanovski <S.I.Nanovski@student.tudelft.nl>
2022-12-02 16:04:42 +0100

    Added the base code for the Activities microservice.

---
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/activities/Application.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.template.activities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Example microservice application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
