1cc2d528769ebc4a1d030d52cef671bfd809cecc
Octavian-Teodor Dragon <dragonteodor@gmail.com>
2022-12-05 20:52:25 +0200

    Fix vague naming for domain paths

--- notifications-microservice/src/main/java/nl/tudelft/sem/project/notifications/config/H2Config.java
+++ notifications-microservice/src/main/java/nl/tudelft/sem/project/notifications/config/H2Config.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * The H2 config.
  */
 @Configuration
-@EnableJpaRepositories("nl.tudelft.sem.template.example.domain")
+@EnableJpaRepositories("nl.tudelft.sem.project.notifications.domain")
 @PropertySource("classpath:application-dev.properties")
 @EnableTransactionManagement
 public class H2Config {
