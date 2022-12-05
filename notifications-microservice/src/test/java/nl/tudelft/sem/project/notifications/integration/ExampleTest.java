76ea9599dd170b6bf0c98f70943ffa5bd23677ae
Octavian-Teodor Dragon <dragonteodor@gmail.com>
2022-12-05 19:47:54 +0200

    Remove authentication tests

--- notifications-microservice/src/test/java/nl/tudelft/sem/project/notifications/integration/ExampleTest.java
+++ notifications-microservice/src/test/java/nl/tudelft/sem/project/notifications/integration/ExampleTest.java
@@ -1,4 +1,4 @@
-package nl.tudelft.sem.template.example.integration;
+package nl.tudelft.sem.project.notifications.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -6,8 +6,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import nl.tudelft.sem.template.example.authentication.AuthManager;
-import nl.tudelft.sem.template.example.authentication.JwtTokenVerifier;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,25 +28,12 @@ public class ExampleTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private transient JwtTokenVerifier mockJwtTokenVerifier;
-
-    @Autowired
-    private transient AuthManager mockAuthenticationManager;
-
-
     @Test
     public void helloWorld() throws Exception {
-        // Arrange
-        // Notice how some custom parts of authorisation need to be mocked.
-        // Otherwise, the integration test would never be able to authorise as the authorisation server is offline.
-        when(mockAuthenticationManager.getNetId()).thenReturn("ExampleUser");
-        when(mockJwtTokenVerifier.validateToken(anyString())).thenReturn(true);
-        when(mockJwtTokenVerifier.getNetIdFromToken(anyString())).thenReturn("ExampleUser");
 
         // Act
         // Still include Bearer token as AuthFilter itself is not mocked
-        ResultActions result = mockMvc.perform(get("/hello")
+        ResultActions result = mockMvc.perform(get("/notifications")
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("Authorization", "Bearer MockedToken"));
 
@@ -57,7 +42,7 @@ public class ExampleTest {
 
         String response = result.andReturn().getResponse().getContentAsString();
 
-        assertThat(response).isEqualTo("Hello ExampleUser");
+        assertThat(response).isEqualTo("[ENDPOINT] Notifications Microservice - WIP");
 
     }
 }
