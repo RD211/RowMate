be475b8ad6be07580068702a1eccda7b46ca1a5c
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-12-10 20:45:01 +0100

    Clean up service

--- users-microservice/src/main/java/nl/tudelft/sem/project/users/domain/certificate/CertificateService.java
+++ users-microservice/src/main/java/nl/tudelft/sem/project/users/domain/certificate/CertificateService.java
@@ -14,30 +14,9 @@ public class CertificateService {
         this.certificateRepository = certificateRepository;
     }
 
-    /**
-     * Register a new user.
-     *
-     * @param netId    The NetID of the user
-     * @param password The password of the user
-     * @throws Exception if the user already exists
-     */
-    public AppUser registerUser(NetId netId, Password password) throws Exception {
+    public Certificate addCertificate() {
 
-        if (checkNetIdIsUnique(netId)) {
-            // Hash password
-            HashedPassword hashedPassword = passwordHashingService.hash(password);
-
-            // Create new account
-            AppUser user = new AppUser(netId, hashedPassword);
-            userRepository.save(user);
-
-            return user;
-        }
-
-        throw new NetIdAlreadyInUseException(netId);
+        return null;
     }
 
-    public boolean checkNetIdIsUnique(NetId netId) {
-        return !userRepository.existsByNetId(netId);
-    }
 }
