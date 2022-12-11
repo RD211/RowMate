0579edeaf8e34347fa4e3f3776a91be1e15e9c3d
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-12-11 23:40:06 +0100

    Try to test repository

--- users-microservice/src/main/java/nl/tudelft/sem/project/users/database/repositories/CertificateRepository.java
+++ users-microservice/src/main/java/nl/tudelft/sem/project/users/database/repositories/CertificateRepository.java
@@ -4,9 +4,11 @@ import nl.tudelft.sem.project.users.domain.certificate.Certificate;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CertificateRepository extends PagingAndSortingRepository<Certificate, UUID> {
+    Optional<Certificate> findByName(String name);
 }
 
--- users-microservice/src/test/java/nl/tudelft/sem/project/users/integration/CertificateTests.java
+++ users-microservice/src/test/java/nl/tudelft/sem/project/users/integration/CertificateTests.java
@@ -2,24 +2,20 @@ package nl.tudelft.sem.project.users.integration;
 
 import nl.tudelft.sem.project.users.database.repositories.CertificateRepository;
 import nl.tudelft.sem.project.users.domain.certificate.Certificate;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.hibernate.SessionFactory;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -34,35 +30,27 @@ public class CertificateTests {
 
     @Test
     public void repositoryStoresRecursiveCertificates() {
+        Configuration configuration = new Configuration().configure();
+        SessionFactory factory = configuration.buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+
         Certificate cert1 = new Certificate("Certificate 1");
         Certificate cert2 = new Certificate("Certificate 2", cert1);
+        session.save(cert1);
+        session.save(cert2);
+        session.getTransaction().commit();
+        session.close();
+
+//        session.
+//
+//        System.out.println(cert1.getId() + " " + savedCert1.getId());
+//
+//        var certFromRepo = certificateRepository.findById(savedCert2.getId());
+//        System.out.println(certFromRepo);
+////
+//        assertThat(certFromRepo.isPresent());
+//        assertThat(certFromRepo.get().hasInChain(savedCert1));
     }
-
-    @Test
-    public void register_withValidData_worksCorrectly() throws Exception {
-        // Arrange
-        final NetId testUser = new NetId("SomeUser");
-        final Password testPassword = new Password("password123");
-        final HashedPassword testHashedPassword = new HashedPassword("hashedTestPassword");
-        when(mockPasswordEncoder.hash(testPassword)).thenReturn(testHashedPassword);
-
-        RegistrationRequestModel model = new RegistrationRequestModel();
-        model.setNetId(testUser.toString());
-        model.setPassword(testPassword.toString());
-
-        // Act
-        ResultActions resultActions = mockMvc.perform(post("/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.serialize(model)));
-
-        // Assert
-        resultActions.andExpect(status().isOk());
-
-        AppUser savedUser = userRepository.findByNetId(testUser).orElseThrow();
-
-        assertThat(savedUser.getNetId()).isEqualTo(testUser);
-        assertThat(savedUser.getPassword()).isEqualTo(testHashedPassword);
-    }
-
 
 }
