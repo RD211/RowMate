a478440109a2026571aa0476a4b1a91bf574dbb7
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-12-23 14:31:38 +0100

    Fix double import

--- matchmaking-microservice/src/main/java/nl/tudelft/sem/project/matchmaking/domain/ActivityRegistrationRepository.java
+++ matchmaking-microservice/src/main/java/nl/tudelft/sem/project/matchmaking/domain/ActivityRegistrationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
